cf5e7caf6cd2a218294bafd4042ae39b002915c1
kcoro <6244044+kcoro@users.noreply.github.com>
2021-03-24 11:02:16 -0400

    updated binarysearch.go

--- binarysearch/binarysearch.go
+++ binarysearch/binarysearch.go
@@ -1,14 +1,12 @@
+package binarysearch
+
 /*
-	binsearch: finds a value using binary search in a sorted list in O(logN)
+	binarysearch finds a value in a sorted list in O(logN).
 	Binary search looks at the middle index of a sorted linear data structure to search
 	for a value.  If the value is not found, binary search checks if the value is higher or lower
 	than the value at the midpoint. Each iteration the value being searched is not found, binary search
 	divides the remaining number of elements to search by 1 / 2.
 */
-
-package binarysearch
-
-/* binsearch: find x value using binary search O(logN) */
 func binarysearch(find int, list []int) int {
 	low := 0
 	high := len(list) - 1
